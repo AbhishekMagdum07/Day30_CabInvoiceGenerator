32183b1c98d78fd7f63267041a99743568afe673
Abhishek <abhishek.magdum.07@gmail.com>
2023-04-11 18:09:55 +0530

    Added : Calculate fare for a cab service

---
+++ src/com/cab_invoice/CabInvoice.java
@@ -0,0 +1,22 @@
+package com.cab_invoice;
+
+import java.util.Scanner;
+
+public class CabInvoice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the distance in km");
+        double distance = sc.nextDouble();
+        System.out.println("Enter the time in minutes.");
+
+        //Uc1 - Calculate Fare.
+        double time = sc.nextDouble();
+        double totalFare;
+
+        double costPerKm = 10;
+        double costPermin = 1;
+        double minFare = 5;
+        totalFare = (distance * costPerKm) + time + minFare;
+        System.out.println("Total Fare is = " + totalFare);
+    }
+}
